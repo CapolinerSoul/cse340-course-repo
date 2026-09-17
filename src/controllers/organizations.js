1cf2f6a36c9cafbde1d59d47acd978ad376bf9bc
CapolinerSoul <francoromanchez@gmail.com>
2026-09-17 16:05:44 -0300

    final fix

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,4 +1,5 @@
-import { getAllOrganizations } from '../models/organizations.js';
+import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
+import { getProjectsByOrganizationId } from '../models/projects.js';
 
 const showOrganizationsPage = async (req, res) => {
     try {
@@ -12,4 +13,14 @@ const showOrganizationsPage = async (req, res) => {
     }
 }
 
-export { showOrganizationsPage }
+const showOrganizationDetailsPage = async (req, res) => {
+    const organizationId = req.params.id;
+    const organizationDetails = await getOrganizationDetails(organizationId);
+    const projects = await getProjectsByOrganizationId(organizationId);
+    const title = 'Organization Details';
+
+    res.render('organization', {title, organizationDetails, projects});
+};
+
+// Export any controller functions
+export { showOrganizationsPage, showOrganizationDetailsPage };
